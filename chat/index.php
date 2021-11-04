55fd392fb0fe18e676f34b61c87aef524a6881f3
Drako-Dev <83632722+Drako-Dev@users.noreply.github.com>
2021-11-04 11:41:27 -0300

    Add files via upload

--- chat/index.php
+++ chat/index.php
@@ -22,6 +22,9 @@
 	<meta charset="utf-8">
 	<title>DRAKO CHAT</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
 	<div class="content">
@@ -29,6 +32,22 @@
 			<div class="chats">
 
 				<?php 
+
+					echo "<div class='chats_header'>
+							<div style='width: 90%'>
+								<div class='profile_pic'>
+									<img style='margin-left: 5px;' src='assets/images/profile_pics/",$_SESSION['user_id'],".png'>
+								</div>
+							</div>
+							<div class='chats_header_options'>
+								<div style='display: flex;'>
+									<a href=''><i class='fas fa-comment-medical'></i></a>
+									<a href=''><i class='fas fa-ellipsis-v'></i></a>
+								</div>
+							</div>
+						  </div>";
+
+					echo "<div style='height: 10px;'></div>";
 
 					include('includes/classes.php');
 
@@ -76,8 +95,11 @@
 
 						echo "<a href='server.php?chat=$f_id'>
 								<div class='one_chat'>
-									<div>",$f_name,"</div>
-									<div>",$f_ultima_msg," <p>$data_formated</p></div>
+									<div class='profile_pic'><img style='margin-left: 5px;' src='assets/images/profile_pics/$f_id.png'></div>
+									<div style='width: 100%;padding: 5px 10px 0px;'>
+										<div>",$f_name,"</div>
+										<div>",$f_ultima_msg," <p>$data_formated</p></div>
+									</div>
 								</div>
 							</a>";
 
@@ -86,9 +108,16 @@
 				?>
 
 			</div>
+			<style type="text/css">
+				.messages{
+					width: 100%;
+				    background-image: url(assets/images/chat_background/bg_dark.jpg);
+					background-repeat: no-repeat;
+				    background-position: center;
+				}
+			</style>
 			<div style="width: 100%;">
-				<div class="messages">
-					
+
 					<?php 
 
 						$pdo = MySql::connect();
@@ -98,6 +127,15 @@
 						$pdo->execute(array($_SESSION['user_id'], @$_SESSION['chat'], @$_SESSION['chat'], $_SESSION['user_id']));
 
 						$pdo = $pdo->fetchAll();
+
+						echo "
+								<div class='chat_header'>
+									<div class='profile_pic'><img src='assets/images/profile_pics/$f_id.png'></div>
+									<div class='chat_header_name'><strong>",$f_name,"</strong></div>
+								</div>
+							";
+
+						echo "<div class='messages'>";
 
 						foreach ($pdo as $key => $value) {
 
@@ -122,11 +160,12 @@
 				</div>
 				<div class="message_inputs">
 				<input type="text" name="msg_input" id="msg" placeholder="Mensagem">
-				<button style="padding: 10px;" type="submit" onclick="send_msg()">ENVIAR !</button>
+				<a href="" onclick="send_msg()"><i class="fas fa-paper-plane"></i></a>
 				</div>
 			</div>
 		</div>
 	</div>
 </body>
 <script type="text/javascript" src="app.js"></script>
+<script src="https://kit.fontawesome.com/bb2a181e2f.js" crossorigin="anonymous"></script>
 </html>
